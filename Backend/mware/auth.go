0ef34806560af825662439d5722c7d9e421574e6
prostosumker <103947462+prostosumker@users.noreply.github.com>
2025-04-30 17:37:57 +0300

    Update auth.go
    немного сократил файл, чтобы четче выполнял задачи

--- Backend/mware/auth.go
+++ Backend/mware/auth.go
@@ -1,21 +1,16 @@
 package middleware
 
 import (
+	"Backend/auth"
+	config "Backend/conf"
 	"net/http"
 	"strings"
 
-	config "Backend/conf"
-
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
 )
 
-var jwtKey = []byte(config.LoadConfig().JWTSecret) // Получаем JWT секрет из конфига
-
-// JWTAuthMiddleware проверяет наличие и валидность JWT токена в запросе
-func JWTAuthMiddleware() gin.HandlerFunc {
+func JWTAuthMiddleware(cfg config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Извлекаем токен из заголовка Authorization
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token required"})
@@ -23,7 +18,6 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		// Токен должен быть в виде: "Bearer <token>"
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization format"})
@@ -33,32 +27,15 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 
 		tokenString := parts[1]
 
-		// Парсим и проверяем токен
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			// Проверяем метод подписи
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, http.ErrAbortHandler
-			}
-			return jwtKey, nil
-		})
-
-		if err != nil || !token.Valid {
+		claims, err := auth.ParseToken(tokenString, []byte(cfg.JWTSecret))
+		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
 			c.Abort()
 			return
 		}
 
-		// Извлекаем информацию из токена, например, ID пользователя
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
-			c.Abort()
-			return
-		}
+		c.Set("user_id", claims.UserID)
 
-		userID := claims["user_id"].(float64)
-		c.Set("user_id", userID) // Сохраняем информацию о пользователе в контексте
-
-		c.Next() // Передаем управление следующему обработчику
+		c.Next()
 	}
 }
