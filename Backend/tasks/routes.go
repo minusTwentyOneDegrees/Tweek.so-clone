83877d557f0b0ef4238e0bf1d3f436a7b803f6d3
prostosumker <103947462+prostosumker@users.noreply.github.com>
2025-05-06 23:01:23 +0300

    stable версия routes.go
    Убрал лишние "/" чтобы маршруты не вели в "/tasks/"

--- Backend/tasks/routes.go
+++ Backend/tasks/routes.go
@@ -4,13 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// RegisterRoutes регистрирует маршруты задач
+//регистрирует маршруты задач
 func RegisterRoutes(rg *gin.RouterGroup, h *Handler) {
 	tasks := rg.Group("/tasks")
 	{
-		tasks.GET("/", h.GetAllTasks)    
-		tasks.POST("/", h.CreateTask)      
-		tasks.PUT("/:id", h.UpdateTask)    
-		tasks.DELETE("/:id", h.DeleteTask) 
+		tasks.GET("", h.GetAllTasks)
+		tasks.POST("", h.CreateTask)
+		tasks.PUT("/:id", h.UpdateTask)
+		tasks.DELETE("/:id", h.DeleteTask)
 	}
 }
