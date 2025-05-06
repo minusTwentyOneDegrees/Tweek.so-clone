dd84adfc1a4cbc97e0ad2d5fb4d501e684bf4430
prostosumker <103947462+prostosumker@users.noreply.github.com>
2025-05-06 22:50:35 +0300

    stable версия model.go
    упразднение идеи подзадач

--- Backend/tasks/model.go
+++ Backend/tasks/model.go
@@ -1,19 +1,9 @@
 package tasks
 
-import "time"
-
-// Subtask — подзадача
-type Subtask struct {
-	ID        int    `json:"id"`        // можно для frontend обновления
-	Title     string `json:"title"`     // текст подзадачи
-	Completed bool   `json:"completed"` // выполнена или нет
-}
-
-// Task — основная задача
 type Task struct {
-	ID        int       `json:"id"`        // ID в БД
-	Title     string    `json:"title"`     // Текст задачи
-	Date      time.Time `json:"date"`      // Дата (день) выполнения задачи
-	Completed bool      `json:"completed"` // Выполнена ли задача в целом
-	Subtasks  []Subtask `json:"subtasks"`  // Массив подзадач
+	ID          int    `json:"id"`          // ID в БД
+	Title       string `json:"title"`       // Текст задачи
+	Description string `json:"description"` // Описание задачи
+	Date        string `json:"date"`        // Дата (день) выполнения задачи
+	Completed   bool   `json:"completed"`   // Выполнена ли задача
 }
