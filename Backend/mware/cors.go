aa00c30d798d7569d3aa612ee52589dd135c2213
prostosumker <103947462+prostosumker@users.noreply.github.com>
2025-05-06 23:07:19 +0300

    stable версия cors.go
    ох и много головной боли ты создавал, хотя и не был виноват

--- Backend/mware/cors.go
+++ Backend/mware/cors.go
@@ -1,13 +1,11 @@
 package middleware
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
-// CORS — разрешает запросы с других доменов (например, с фронта на localhost:3000)
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
