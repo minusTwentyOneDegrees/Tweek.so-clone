678a47ba018d57738031671abfb7e5771c85cb3f
prostosumker <103947462+prostosumker@users.noreply.github.com>
2025-06-06 16:49:30 +0300

    Update auth.go

--- Backend/pkg/middleware/auth.go
+++ Backend/pkg/middleware/auth.go
@@ -44,3 +44,18 @@ func JWTAuthMiddleware(cfg config.Config) gin.HandlerFunc {
 		c.Next()
 	}
 }
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
