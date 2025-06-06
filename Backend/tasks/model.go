434b59b371f2d489530c92cdbb42e3a4fe61db65
prostosumker <103947462+prostosumker@users.noreply.github.com>
2025-06-06 17:06:35 +0300

    Delete Backend/tasks directory

--- Backend/tasks/model.go
+++
@@ -1,9 +0,0 @@
-package tasks
-
-type Task struct {
-	ID          int    `json:"id"`          // ID в БД
-	Title       string `json:"title"`       // Текст задачи
-	Description string `json:"description"` // Описание задачи
-	Date        string `json:"date"`        // Дата (день) выполнения задачи
-	Completed   bool   `json:"completed"`   // Выполнена ли задача
-}
